829bcc53c7710c96b9b667c39bd7ca73ef516fcc
Pranav Mhalaskar <89830266+PranavM4601@users.noreply.github.com>
2025-03-18 11:47:14 +0530

    Add files via upload

---
+++ src/constants/index.js
@@ -0,0 +1,65 @@
+import project1 from "../assets/projects/project-1.jpg";
+import project2 from "../assets/projects/project-2.jpg";
+import project4 from "../assets/projects/project-4.jpeg";
+import project5 from "../assets/projects/project-5.jpeg";
+import project6 from "../assets/projects/project-6.jpeg";
+
+export const HERO_CONTENT = `I am a passionate Software developer with a knack for crafting robust and scalable products/applications. With experience uptil now, I have honed my skills in various technologies like Java, C++, Python, HTML, CSS, MySQL, as well as recent learnings like Node.js, Express.js, Tailwind Css, React.js, MongoDB. Other than that, I also have experience in Android development using Android Studio, Firebase, Java, Python, XML and UI/UX designing with Figma too. My goal is to join a dynamic firm where, I can learn new technologies from industry experts and gain valuable experience for future betterment. Thereby leverage my expertise to create innovative solutions that drive business/firm's growth and deliver exceptional user experiences.`;
+
+export const ABOUT_TEXT = `🌟👋 Hello, I'm Pranav Mhalaskar, a Computer Science graduate from Smt. Kashibai Navale College of Engineering (SKNCOE) Pune, with a solid foundation in computer science and a deep passion for creating impactful tech solutions. I am a dedicated and passionate about Java programming, with expertise in Java SD and a growing interest in Java EE technologies. Excited to apply my technical skills to real-world challenges with a passion for creating efficient and user-friendly web applications. With experience until now , I have worked with a variety of technologies, including Node.js, Express.js, Tailwind Css, React.js, MySQL and MongoDB. My journey in the development began with a deep curiosity for how things work, and it has evolved into a career where I continuously strive to learn and adapt to new challenges. I thrive in collaborative environments and enjoy solving complex problems to deliver high-quality solutions. Outside of coding, I enjoy staying active, exploring new technologies, contributing to open-source projects and playing football Cricket / Vedio-Games.🌟`;
+
+export const EXPERIENCES = [
+  {
+    year: "FEB 24 - SEP 24",
+    role: "Java Developer ",
+    company: "Vintyaa Technologies Pvt Ltd",
+    description: ` Assisted in code reviews and developed an automation dashboard using Java, MySQL, Spring MVC, and
+    ML/AI. Optimized automation processes, enhancing workflow, integrations, and OCR/YOLO-V5 with ML/AI.`,
+    technologies: ["Java", "SpringBoot", "SpringMVC", "REST-API", "YOLO-V5", "Nodejs", "Html","CSS","UI Development","MySQL","AI","ML","OCR" ],
+  },
+
+];
+
+export const PROJECTS = [
+  {
+    title: "Smart-Eye",
+    image: project2,
+    description:
+      "Developed a fully functional android application leveraging facial and number plate recognition for enhanced security.",
+    technologies: ["Android Studio", "Python", "Java","Xml", "Firebase", "Figma"],
+  },
+  {
+    title: "Employee Attrition: Data Analysis",
+    image: project1,
+    description:
+      " Built a data-driven solution using Machine Learning and Python to predict and analyze employee attrition.",
+    technologies: ["Data Analysis", "Python", "AI", "ML", "Jupyter-Notebook"],
+  },
+  {
+    title: "Post's WEB-APP",
+    image: project4,
+    description:
+      "Developed a web app for user registration, login, secure login/logout and personalized post creation with like/edit features.",
+    technologies: ["HTML", "CSS", "Javascript", "Node.js","Express.js","MongoDB"],
+  },
+  {
+    title: "Employee Management System",
+    image: project5,
+    description:
+      "Built a responsive EMS using React.js and Tailwind CSS with role-based dashboards with task management, status tracking, and a comment section for a seamless user experience ",
+    technologies: ["React.js", "Tailwind CSS"],
+  },
+  {
+    title: "Vehicle Parking Management System",
+    image: project6,
+    description:
+      " Developed a C++ based management system for efficient data maintenance and financial tracking with CRUD (Create, Read, Update, Delete) operations and Efficient File Handling",
+    technologies: ["C++", "File Handling", "OOP's"],
+  },
+];
+
+export const CONTACT = {
+  address: "Bharati Vidyapeeth, Pune ",
+  phoneNo: "+91 9527270792 ",
+  email: "pranavmhalaskar@gmail.com",
+};
